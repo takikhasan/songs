90eeaf0cb90874cb3967475a98d91ce4c3e88c58
Takik Hasan <hasantakik@gmail.com>
2020-01-22 18:48:07 +0600

    Updates

---
+++ precache-manifest.6ec9598c7b4b1232cd9ee5ab5926bb58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78c6e21f90b609b44f94bc446139fcb",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "fdeff4fd66817c0c352e",
+    "url": "/songs/static/js/2.3e9c0c66.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/songs/static/js/2.3e9c0c66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6be747a46b82680354a7",
+    "url": "/songs/static/js/main.56321e4f.chunk.js"
+  },
+  {
+    "revision": "fc8dcd007500c0fd04ce",
+    "url": "/songs/static/js/runtime-main.91e7c810.js"
+  }
+]);
